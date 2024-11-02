33af0fbbfb4041e3c1539575206400e92a0ef955
PudCheetah <wer212131@gmail.com>
2024-11-02 20:32:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mypratice_flow_ui_componments/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mypratice_flow_ui_componments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mypratice_flow_ui_componments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/test/java/com/example/mypratice_flow_ui_componments/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.mypratice_flow_ui_componments
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
